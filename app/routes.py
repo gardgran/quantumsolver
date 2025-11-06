fa4edeecda6595d1810ca8c29319b21546d379ae
Jason Wright <jason@thought.net>
2025-11-06 13:54:56 -0700

    wire in the bz classical solver

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,11 @@
+'''This is the flask router for the quantum solver app'''
 
-from app import app
 from flask import request
+from app import app
 import DeutschClassical
+import bernstein_vazirani_classical
+
+# pylint: disable=missing-function-docstring
 
 @app.route('/')
 @app.route('/index')
@@ -9,8 +13,15 @@ def index():
     return "Welcom to the quantum solver."
 
 @app.route('/deutsch-classical', methods=['POST'])
-def deutsch():
+def deutschclassical():
     if not request.is_json:
         return "expected json input"
     data = request.json
     return DeutschClassical.solve(data)
+
+@app.route('/bernstein-vazirani-classical', methods=['POST'])
+def bz_classical():
+    if not request.is_json:
+        return "expected json input"
+    data = request.json
+    return bernstein_vazirani_classical.solve(data)
