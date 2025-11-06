24929d96a447187f7b2cfbf66a6cd471e1fb98ef
Jason Wright <jason@thought.net>
2025-11-06 08:45:43 -0700

    implementation of deutsch classical solver

---
+++ DeutschClassical.py
@@ -0,0 +1,14 @@
+
+'''This is a classical solver for the Deutsch problem'''
+
+def solve(data):
+    """Given a problem instance in json format, return a json result"""
+    
+    """We expect the incoming json schema to be a list of length two.
+       Each entry is either true or false. The first number represents
+       f(false), and the second represents f(true)"""
+    if data[0] == data[1]:
+        result = {"answer": "constant"}
+    else:
+        result = {"answer": "balanced"}
+    return result
--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,16 @@
 
 from app import app
 from flask import request
-import Deutsch
+import DeutschClassical
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Welcom to the quantum solver."
 
-@app.route('/deutsch', methods=['POST'])
+@app.route('/deutsch-classical', methods=['POST'])
 def deutsch():
     if not request.is_json:
         return "expected json input"
     data = request.json
-    return Deutsch.solve(data)
+    return DeutschClassical.solve(data)
