be0a71abf769ac5305b2bea9b9a613910c468841
Jason Wright <jason@thought.net>
2025-11-09 20:05:03 -0700

    cleanup function defs

--- deutsch_classical.py
+++ deutsch_classical.py
@@ -3,9 +3,12 @@
 
 def solve(data: list) -> dict:
     """We expect the incoming json schema to be a list of length two.
-    Each entry is either true or false. The first number represents
-    f(false), and the second represents f(true)
+    Each entry is either 0 or 1. The first number represents
+    f(0), and the second represents f(1)
     """
+
+    # convert input data to booleans
+    data = [bool(x) for x in data]
 
     def f(x: bool) -> bool:
         """This is the hidden function f we are trying to analyze"""
