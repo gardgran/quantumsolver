0fa19b89ab7a353b1e922504452316b9a240e96d
George Lake <georgelake2@isu.edu>
2025-12-11 08:23:13 -0700

    Start on Shors

---
+++ shors.py
@@ -0,0 +1,12 @@
+"""
+Shor's Algorithm Quantum Solver
+
+This is a quantum solver simulation for shor's algorthim.
+The quantum simulation is done during the Quantum Phase Estimation (QPE) to find the period.
+Once the period is found. The factors of N are classically solved.
+
+The quantum simulation only works (in a reasonable time frame) for small numbers ( < 7 bits). 
+Above that, the program will not attempt to run.
+
+George Lake - 12/2025
+"""
